752f6750d350365604b99d938f424b35119b39a4
pedroenrico <pedroenricoferraz@gmail.com>
2023-01-23 18:42:18 -0300

    Delete the coments in the javascript codes

--- script.js
+++ script.js
@@ -8,11 +8,6 @@ form.addEventListener("change", save)
 function add() {
   const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
 
-  /*Abaixo está como adicionar na biblioteca um dia que não é o de hoje (Obs: Devemos prencher no mínimo um dos hábitos desse dia passsado para guardar os que foram prenchidos)*/
-  /*const today = "18/01"*/
-  /*const today = "19/01"*/
-  /*Para cada navegador e sua máquina há um progresso e localstorage de marcações para fazer ou feitas na última vez  que esta página foi acessada*/
-
   const dayExists = nlwSetup.dayExists(today)
 
   if (dayExists) {
@@ -27,14 +22,6 @@ function add() {
 function save() {
   localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
-/*const data = {
-  banhogelado: ["01-01", "01-02", "01-03", "01-04", "01-05"],
-  banhodesol: ["01-02", "01-03", "01-05"],
-  treino: ["01-01", "01-03", "01-04"],
-  leitura: ["01-02", "01-03", "01-05"],
-  programação: ["01-02", "01-04", "01-05"],
-  sonodequalidade: ["01-01", "01-03"],
-}*/
 
 const data = JSON.parse(localStorage.getItem("NLWSetup@habits"))
 nlwSetup.setData(data)
